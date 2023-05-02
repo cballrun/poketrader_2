92d6dc2dea3abd48d4c601e78fd80b7336351c9d
Carter Ball <carter.k.ball@gmail.com>
2023-05-02 01:03:44 -0400

    Card facade get cards by name working

--- app/facades/card_facade.rb
+++ app/facades/card_facade.rb
@@ -1,8 +1,16 @@
+
 class CardFacade
   
   def self.card_by_id(card_id)
     card = CardService.get_card_by_id(card_id)
     Card.new(card[:data])
+  end
+
+  def self.get_cards_by_name(name)
+    cards= CardService.search_cards_by_name(name)
+    cards[:data].map do |card|
+      Card.new(card)
+    end
   end
 
 end
--- spec/facades/card_facade_spec.rb
+++ spec/facades/card_facade_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 RSpec.describe CardFacade do
 
   describe '#card_by_id' do
-    it 'returns a single movie', vcr: { record: :new_episodes } do
+    it 'returns a single card by id', vcr: { record: :new_episodes } do
       card_id = "base4-1"
 
       card = CardFacade.card_by_id(card_id)
@@ -19,6 +19,29 @@ RSpec.describe CardFacade do
       expect(card.set_name).to be_a(String)
       expect(card.supertype).to be_a(String)
 
+    end
+  end
+
+  describe '#get_cards_by_name' do
+    it 'returns cards based on a full name search', vcr: { record: :new_episodes } do
+      card_name = "gengar"
+
+      cards = CardFacade.get_cards_by_name(card_name)
+
+      expect(cards).to be_a(Array)
+      expect(cards.count).to be_a(Integer)      
+
+      cards.each do |card|
+        expect(card).to be_a(Card)
+        expect(card.card_id).to be_a(String)
+        expect(card.card_name).to be_a(String)
+        expect(card.image).to be_a(String)
+        expect(card.market_value).to be_a(Float)
+        expect(card.rarity).to be_a(String)
+        expect(card.series_name).to be_a(String)
+        expect(card.set_name).to be_a(String)
+        expect(card.supertype).to be_a(String)
+      end
     end
   end
 
