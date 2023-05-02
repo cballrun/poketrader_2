43d111bd168d116421e2d3ea8288c411b0f5578b
Carter Ball <carter.k.ball@gmail.com>
2023-05-02 16:29:57 -0400

    Additional search index test, need to add more granular tests in future

--- spec/features/cards/index_spec.rb
+++ spec/features/cards/index_spec.rb
@@ -9,13 +9,24 @@ RSpec.describe 'the Card Search Index Page', vcr: { record: :new_episodes }  do
   end
 
   context 'card search by name' do
-    describe 'after the search field is filled out and submitted' do
+    describe 'the search landing page after the search field is submitted' do
       it 'returns cards where the name includes the search term' do
         fill_in 'Card Search', with: 'wurmple'
         click_button 'Search'
-        
-        expect(page).to have_content('Wurmple')
+        within "#card_table" do
+          expect(page).to have_content('Wurmple')
+          
+        end
+      end
 
+      it 'has columns for series, set, and market value' do
+        fill_in 'Card Search', with: 'wurmple'
+        click_button 'Search'
+        within "#card_table" do
+          expect(page).to have_content('Series')
+          expect(page).to have_content('Set')
+          expect(page).to have_content('Name')
+        end
       end
     end
   end
